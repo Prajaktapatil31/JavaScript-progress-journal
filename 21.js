d0c7540e3d6fda775bfb1aba44a664e812a9cb1c
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-27 21:52:28 +0530

    initial commit

---
+++ 21.js
@@ -0,0 +1,18 @@
+let action = Number(prompt(`
+        ATM Action Menu
+    ------------------------   
+    
+    1. Check Balance
+    2. Withdraw 
+    3. Deposit
+    4. Exit
+`))
+
+switch(action){
+    case 1: console.log('Check Balance'); break;
+    case 1: console.log('Withdraw '); break;
+    case 1: console.log('Deposit'); break;
+    case 1: console.log('Exit'); break;
+    default: console.log('invalid action choosen');
+        
+}
