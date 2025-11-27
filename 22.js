7f474630932587da76e7632067c49f415016f1d2
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-27 22:04:19 +0530

    Update 22.js

---
+++ 22.js
@@ -0,0 +1,40 @@
+/*
+
+----------------------- Loops in JavaScript --------------------------
+
+Entry-controlled Loops: 
+    Condition is checked before executing the loopbody (e.g., for, while).
+
+Exit-controlled Loops: 
+    Loop body executes first, then condition is checked(e.g., do...while).
+
+
+
+  Types of Loops
+--------------------
+
+1. for loop
+2. while loop
+3. do-while loop
+4. for...of Loop
+5. for in loop
+
+
+for...of Loop : Used to loop over iterables like arrays, strings, etc.
+
+Syntax:
+const fruits = ["apple", "banana", "cherry"];
+for (const fruit of fruits) [
+    console.log(fruit);
+}
+
+  
+*/
+
+
+
+
+
+
+
+
