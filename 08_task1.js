b9605a879e9d66690d34b482ae43c417e7df9016
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-27 21:50:23 +0530

    initial commit

---
+++ 08_task1.js
@@ -0,0 +1,20 @@
+//Excersice
+let name="piu"
+let age=10
+let isStudent=true
+
+console.log(name,age,isStudent);    //piu 10 true
+
+console.log(
+    `
+    Name = ${name}
+    Age = ${age}
+    isStudent = ${isStudent}
+    `
+);
+
+/*
+    Name = piu
+    Age = 10
+    isStudent = true
+ */
