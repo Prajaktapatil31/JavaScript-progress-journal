7e103ce01fb1068118eb71663ebfe1e69030d3a0
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-27 22:05:26 +0530

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,2 +1,16 @@
+/*
+
+---------------------------  What Is JavaScript?  ---------------------------------------
+
+JavaScript is a popular programming language used to create interactive effects on websites.
+It runs directly in web browsers like Chrome, Firefox, and Safari.
+JavaScript enables features like image sliders, form validation, pop-ups, and animations.
+It works together with HTML (structure) and CSS (style) to build dynamic web pages.
+
+*/
+
+
+
 // external javascript
+
 document.write("welcome")
