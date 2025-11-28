4fd9460ec21122d4919266105ebdd4712042dce9
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-28 17:19:54 +0530

    initial commit

---
+++ 31Array.js
@@ -0,0 +1,31 @@
+/*
+---------------------- Array ------------------------
+Array is used to store multiple values in a single variable.
+It is mutable/modifiable datatype in Js.
+array index/key pe kam krta hai.
+Array mein different types ki values ko store kr sakte hai 
+-strings
+-numbers
+-objects
+-other arrays
+*/
+
+
+let arr=[2,4,5,2,'apple',6.2,false,'r']
+console.log(arr[3]); //2
+
+
+let users = ['ram','ravi','raj','dev','sai']
+for(let x of users){
+    console.log(x);
+    
+}
+
+/*
+ram
+ravi
+raj
+dev
+sai
+*/
+
