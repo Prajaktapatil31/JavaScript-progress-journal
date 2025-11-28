9b848cb6554f35c4cee5817bc4ccd2279f940c36
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-28 17:17:53 +0530

    initial commit

---
+++ 24.js
@@ -0,0 +1,9 @@
+//Sum of first 10 Natural Numbers 
+ 
+let sum=0
+
+for(let i=1;i<=10;i++){
+    sum+=i;
+}
+
+console.log(sum); //55
---
+++ 25.js
@@ -0,0 +1,26 @@
+//Nested Loop
+
+document.write("square star pattern <br>");
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        document.write("* ");
+        
+    }
+    document.write("<br>");
+}
+
+
+/*
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
+
+
+//break - exit the loop
+//continue - skip current iteration
