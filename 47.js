e0919de1347201b0b9a6cf6f9fde12de50a79f8e
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-29 19:01:55 +0530

    initial commit

---
+++ 47.js
@@ -0,0 +1,17 @@
+//function with parameters
+
+function add(a,b){
+    console.log(a+b);
+}
+add(2,3) //5
+
+// ----------------------------------------------------------------
+
+function showName(user){
+    console.log(`welcome ${user}`)
+}
+
+showName('prajakta')  //welcome prajakta 
+
+// user --> argument (passed at the time of function defining)
+// prajakta --> parameter (passed at the time of function calling)
