25ad547e90aa6b884a8084350313b3eebf00605f
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-29 19:01:10 +0530

    initial commit

---
+++ 44_Functions.js
@@ -0,0 +1,19 @@
+/* 
+------------------  Functions in JavaScript  ----------------------
+
+If we want to perform some task repeadly,then we can use function.
+It is a block of code used to perform a particular task.
+
+Types :
+1.Inbuilt function      eg. prompt, alert
+2.User define function
+*/
+
+
+function showData(){
+    console.log('welcome');
+    // document.write('hello')
+}
+
+showData()  //function call
+//welcome
