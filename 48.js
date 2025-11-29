cc490691a7b3042a2a8248758658581bb4928b81
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-29 19:02:10 +0530

    initial commit

---
+++ 48.js
@@ -0,0 +1,19 @@
+function addData(num1,num2){
+    console.log(num1+num2);    
+    console.log(num1);    
+    console.log(num2);    
+}
+
+addData(10)
+/*
+NaN
+10
+undefined
+*/
+
+addData(10,5,4)
+/*
+15
+10
+5
+*/
