ffa0aaf8a91718ad34c82c2d689117d25cfdffa9
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-11-29 19:01:23 +0530

    initial commit

---
+++ 45.js
@@ -0,0 +1,9 @@
+//function keyword ka use krke jo function banate hai, to wahapr hoisting ka concept use hota hai
+//jb code execute hota hai, jo function niche define kiya hota hai.. vo upr jake define ho jata hai
+//before function calling, function defination top pe chale jata hai
+
+sum() //3
+
+function sum(){
+    console.log(1+2);
+}
