9020f20a6eddb3af44364a7c378c8cd2a9a87b33
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-12-10 19:35:41 +0530

    Add files via upload

---
+++ 82.js
@@ -0,0 +1,5 @@
+//------------   Select Element by id  ----------------
+let h = document.getElementById('heading')
+console.log(h);
+let paragraph = document.getElementById('para1')
+console.log(paragraph);
