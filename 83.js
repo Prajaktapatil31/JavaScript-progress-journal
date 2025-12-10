9d07ca9774d466bfd500b6a43254d8d20e1d44ad
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-12-10 19:36:04 +0530

    Add files via upload

---
+++ 83.js
@@ -0,0 +1,11 @@
+/*         innerText vs innerHTML
+---------------------------------------------
+| Property      |  Description              |
+|-------------------------------------------| 
+| innerText     |  returns plain text       |
+| innerHTML     | returns plain text + tags | 
+---------------------------------------------     */
+
+let h = document.getElementById('heading')
+console.log(h.innerHTML);
+console.log(h.innerText);
