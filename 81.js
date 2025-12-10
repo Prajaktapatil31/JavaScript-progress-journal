b42dc751477fd922e272545dbe887b3af70e27c1
Prajakta Patil <118681707+Prajaktapatil31@users.noreply.github.com>
2025-12-10 19:35:21 +0530

    Add files via upload

---
+++ 81.js
@@ -0,0 +1,15 @@
+/* ---------- Document Object Model in JS ----------
+DOM stands for Document Object Model
+
+It's a tree-like structure created by the browser from an HTML page.Every HTML element is treated as an object in the DOM.
+
+JavaScript can be used to:
+    --> Access elements
+    --> Modify content
+    --> Add/remove classes or styles
+    --> Handle user events
+*/
+
+console.log(document);
+console.log(window);
+
